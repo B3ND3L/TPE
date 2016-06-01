86675ff664fb317d193ae2add31b831528e95044
lemazo <delahais.b@gmail.com>
2016-06-01 14:15:13 +0200

    ajout du placement des cabanes

--- src/parser.js
+++ src/parser.js
@@ -16,17 +16,29 @@ function getCabanes(){
 	
 	return array;*/
 	
-	return lireJSon();
+	return connectPhp();
+	
+	//return lireJSon();
 	
 }
 
-function lireJSon(){
+function connectPhp(){
+	
+	$.post( "php/connexionAjax.php", { request : "crypto" })
+	.done(function( data ) {
+		//alert( "Data Received" );
+		tabCabane=lireJSon(data);
+	});
+}
 
-	var json = '{"cabanes":[{"x":0,"y":0,"coloration":"coucou"},{"x":3,"y":3,"coloration":"youhou"}]}';
-	var o = jQuery.parseJSON(json).cabanes;
-	console.log(o);
+function lireJSon(json){
+
+	//var json = '{"cabanes":[{"x":0,"y":0,"coloration":"coucou"},{"x":3,"y":3,"coloration":"youhou"}]}';
+	console.log(json);
+	var arrayCabane = jQuery.parseJSON(json).cabanes;
+	//console.log(arrayCabane);
 	/*o.forEach(function(v){
 		console.log(v);
 	});*/
-	return o;
+	return arrayCabane;
 }
